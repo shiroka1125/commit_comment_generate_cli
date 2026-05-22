2814ddafbe257de2b0ccfac1b05035ea17d16985
takagi <shiroka1125@gmail.com>
2026-05-22 22:51:10 +0900

    fix

--- src/commit_comment_generate_cli/main.py
+++ src/commit_comment_generate_cli/main.py
@@ -12,6 +12,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[logging.FileHandler("ccg.log")],
+    encoding="utf-8",
 )
 
 logger = logging.getLogger(__name__)
